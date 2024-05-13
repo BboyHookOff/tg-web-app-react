aa61015dd383248cbd970e34a0d717f16674b171
BboyHookOff <kryuchkov.grisha@gmail.com>
2024-05-13 03:44:01 +0300

    commit 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
+import Header from './components/header/Header';
 import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
